991eaad6311174c0ee0ef9df1bfce21826454b3a
mylhyz <wangyu4@huya.com>
2020-04-17 16:50:13 +0800

    build bsdiff/bspatch shared library for android

--- android/bsdiff/src/main/jni/Android.mk
+++ android/bsdiff/src/main/jni/Android.mk
@@ -2,6 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := bzip2
 BZIP2_ROOT_PATH := ${LOCAL_PATH}/../../../../../bzip2-1.0.8/
+LOCAL_EXPORT_C_INCLUDES := $(BZIP2_ROOT_PATH)
 LOCAL_SRC_FILES := ${BZIP2_ROOT_PATH}/bzlib.h   \
                    ${BZIP2_ROOT_PATH}/bzlib.c   \
                    ${BZIP2_ROOT_PATH}/bzlib_private.h   \
@@ -11,4 +12,11 @@ LOCAL_SRC_FILES := ${BZIP2_ROOT_PATH}/bzlib.h   \
                    ${BZIP2_ROOT_PATH}/decompress.c  \
                    ${BZIP2_ROOT_PATH}/crctable.c   \
                    ${BZIP2_ROOT_PATH}/randtable.c
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := bsdiff
+LOCAL_SHARED_LIBRARIES := bzip2
+LOCAL_SRC_FILES := ${LOCAL_PATH}/libbsdiff.c \
+                   ${LOCAL_PATH}/libbspatch.c
 include $(BUILD_SHARED_LIBRARY)
